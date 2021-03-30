b66280a60ae63ed41553a9edd3b53fe4df302e96
Steven Andrews <sandrews4862@gmail.com>
2021-03-30 09:44:34 -0400

    Add gitignore

--- Pods/Target Support Files/Pods-Wagr-Assignment-Wagr-AssignmentUITests/Pods-Wagr-Assignment-Wagr-AssignmentUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Wagr_Assignment_Wagr_AssignmentUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Wagr_Assignment_Wagr_AssignmentUITests
-@end
--- Pods/Target Support Files/Pods-Wagr-AssignmentTests/Pods-Wagr-AssignmentTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Wagr_AssignmentTests : NSObject
-@end
-@implementation PodsDummy_Pods_Wagr_AssignmentTests
-@end
